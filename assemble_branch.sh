af93a226fbcb315260a6b532168e480bb2392e5d
ubuntu <ubuntu@host.com>
2021-10-23 19:03:52 +0800

    opt

--- assemble_branch.sh
+++ assemble_branch.sh
@@ -6,5 +6,6 @@ fi
 cd /home/ubuntu/mgcnode/node &&
 rm -rf out &&
 git checkout $1 &&
+git reset --hard HEAD &&
 ./androidbuild/build-android-node /home/ubuntu/android-ndk-r21e &&
 cp out/Release/lib/libnode.so /home/ubuntu/shared_with_host &&echo "assemble libnode done!"
